29f494913ae83a68b3b368ffe9ab253a1e7bd708
Viktor Břenek <70347594+viktorbrenek@users.noreply.github.com>
2022-09-07 11:00:48 +0200

    lessons

---
+++ docs/exercise/exersise04/src/script.js
@@ -0,0 +1,30 @@
+import './style.css'
+import * as THREE from "three"
+
+//scene
+const scene = new THREE.Scene()
+
+//redcube
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+const sizes = {
+    width: 800,
+    height: 600
+}
+//camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+camera.position.x = 2
+camera.position.y = 2
+scene.add(camera)
+
+const canvas = document.querySelector(".webgl")
+const renderer = new THREE.WebGLRenderer ({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+
+renderer.render(scene, camera)
